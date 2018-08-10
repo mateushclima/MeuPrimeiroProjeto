f0fc97b89706887aad79b7963e1bac5844ff4507
Mateus Henrique Cardoso Lima <mateushclima@gmail.com>
2018-08-10 11:13:35 -0300

    Implementado a tabuada do 6.

--- src/meuprimeiroprojeto/MeuPrimeiroProjeto.java
+++ src/meuprimeiroprojeto/MeuPrimeiroProjeto.java
@@ -15,7 +15,9 @@ public class MeuPrimeiroProjeto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       for (int i = 0; i<=10; i++){
+           System.out.println(i*6);
+       }
     }
     
 }
